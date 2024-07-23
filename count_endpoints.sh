8b22052767bd9ed0984a8d1503acbf7704a0c33c
abhinvv1 <abhinav.p@browserstack.com>
2024-07-23 19:23:58 +0530

    update to remove extra characters

--- count_endpoints.sh
+++ count_endpoints.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-
-map_endpoints() {
-    awk -F':' '
-    $3 ~ /Started/ {
-        split($3, parts, " ")
-        method = parts[2]
-        endpoint = parts[3]
-	sub(/\?.*/, "", endpoint)
-	print method, endpoint
-    }
-    ' "$1"
-}
-
-reduce_counts() {
-   sort | uniq -c |  awk '{print $2, $3, $1}'
-}
-
-map_endpoints tmp_more_than_50 | reduce_counts
-
+awk -F: '$3 ~ /Started/ {split($3,a," ");sub(/\?.*/,"",a[3]);print a[2],a[3]}' "$1" | sort | uniq -c | awk '{print $2, $3, $1}'
 
