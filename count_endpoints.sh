700c9bd7f3e5318e95b074800ab8b5404ed1f48c
abhinvv1 <abhinav.p@browserstack.com>
2024-07-23 20:00:26 +0530

    update script to follow output format

--- count_endpoints.sh
+++ count_endpoints.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-awk -F: '$3 ~ /Started/ {split($3,a," ");sub(/\?.*/,"",a[3]);print a[2],a[3]}' "$1" | sort | uniq -c | awk '{print $2, $3, $1}'
-
+awk -F: '$3 ~ /Started/ {split($3,a," ");sub(/\?.*/,"",a[3]);print a[2],a[3]}' "$1" | sort | uniq -c | awk '{print $1,  $2, $3}' | sort -rn
