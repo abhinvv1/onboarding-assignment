debaa036432f8970d15c6367b7f9780a3cebb8a2
abhinvv1 <abhinav.p@browserstack.com>
2024-08-01 20:42:27 +0530

    update email validation regex, password length
    - fix welcome message popping up on failed singup

--- file_sharing_app_with_gems/app/controllers/users/registrations_controller.rb
+++ file_sharing_app_with_gems/app/controllers/users/registrations_controller.rb
@@ -5,11 +5,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_sign_up_params
-    flash[:notice] = "Welcome! Your account has been created successfully."
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :email])
   end
+
   def configure_account_update_params
-    flash[:notice] = "Thanks for signing up! Please check your email to confirm your account."
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :email])
+  end
+
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "Welcome! Your account has been created successfully."
+    super(resource)
   end
 end
